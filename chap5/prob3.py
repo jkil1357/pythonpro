d7e0cc259b758edbf1be254ad38c1ca25077145e
jkil1357 <65268298+jkil1357@users.noreply.github.com>
2023-10-11 00:26:16 +0900

    chap5

--- chap5/prob3.py
+++ chap5/prob3.py
@@ -1,21 +1,29 @@
 import random
 
 words = ("difficult", "banana", "apple", "python", "daegu", "catholic", "university")
+print("Guess the Word!!!")
 print("In this game, the program selects a word at random, and the player's objective is to guess the chosen word.\n")
 ch = random.choice(words)
-
-print("Length of the selected word:", len(ch))
-guess = None
-att = len(ch)
-a = 1
 print(ch)
 
-while guess != ch:
+word = ["_"] * len(ch)
+att = len(ch)
+print("Length of the selected word:", len(ch))
+
+while '_' in word and att > 0:
     print("Remaining attempts:", att)
-    print("Current guessed word: ", end='')
-    while a <= len(ch):
-        print("_ ", end='')
-        a += 1
-    guess = input("\nGuess a letter: ")
+    print("Current guessed word:",' '.join(word))
+    guess = input("Guess a letter: ")
+
     if guess in ch:
-        print("")
+        for i in range(len(ch)):
+            if ch[i] == guess:
+                word[i] = guess
+    else:
+        print("Incorrect guess.")
+        att -= 1
+    
+    if '_' not in word:
+        print("Congratulations! You guessed the word:", ch)
+    elif att == 0:
+        print("You've used up all your attempts. The correct word was", ch)
