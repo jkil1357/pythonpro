a9e140aad303c01ad8178af8b976b1ce68476824
jkil1357 <65268298+jkil1357@users.noreply.github.com>
2023-12-04 23:26:22 +0900

    chap10

--- chap10/prob1.py
+++ chap10/prob1.py
@@ -1,1 +1,18 @@
+class Player:
+    def blast(self, enemy):
+        print("The player blasts an enemy.")
+        enemy.die()
 
+class Alien:
+    def die(self):
+        print("\nThe alien gasps and says, 'Oh, this is it. This is the big one.")
+        print("Yes, it's getting dark now. Tell my 1.6 million larvae that I loved them...")
+        print("Good-bye, cruel universe.")
+
+print("\t\tDeath of an Alien\n")
+
+hero = Player()
+invader = Alien()
+hero.blast(invader)
+
+input("\n\nPress the enter key to exit.")
