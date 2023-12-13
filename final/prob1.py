2863c2ff799e511413634605017064b8de71eb1a
jkil1357 <65268298+jkil1357@users.noreply.github.com>
2023-12-13 09:49:25 +0900

    Update prob1.py

--- final/prob1.py
+++ final/prob1.py
@@ -1,1 +1,14 @@
+def Frequency_analytic(s):
+    dic = {}
+    for i in s:
+        dic[i] = dic.get(i,0)+1
+    for key, value in sorted(dic.items()):
+        if str.islower(key):
+            print(key, value)
+    for key, value in sorted(dic.items()):
+            if str.isupper(key):
+                print(key, value)
 
+if __name__ == "__main__":
+    msg = input("input your message : ")
+    Frequency_analytic(msg)
